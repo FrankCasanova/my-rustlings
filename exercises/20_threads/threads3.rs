480af5d5ecb24b172cfbaced334b34347c1e9962
Frank Casanova <frankcasanova.info@gmail.com>
2025-05-09 17:46:31 +0200

    fix(macros): correct macro syntax and usage in exercises
    This commit addresses several issues in the macros exercises, including:
    - Adding missing semicolons in macro definitions
    - Fixing macro calls by ensuring proper syntax
    - Adding `#[macro_use]` attribute to make macros accessible
    - Moving macro definitions before their usage to avoid compiler errors

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -19,7 +19,8 @@ impl Queue {
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     // TODO: We want to send `tx` to both threads. But currently, it is moved
     // into the first thread. How could you solve this problem?
-    thread::spawn(move || {
+    let tx2 = tx.clone();
+       thread::spawn(move || {
         for val in q.first_half {
             println!("Sending {val:?}");
             tx.send(val).unwrap();
@@ -30,7 +31,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     thread::spawn(move || {
         for val in q.second_half {
             println!("Sending {val:?}");
-            tx.send(val).unwrap();
+            tx2.send(val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
